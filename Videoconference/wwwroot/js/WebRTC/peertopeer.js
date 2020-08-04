9009711f976931b40d7cf510b6eb45332199d4e7
laisne.gautier <laisne.gautier@gmail.com>
2020-08-04 16:03:39 +0200

    Removing legacy code

--- Videoconference/wwwroot/js/WebRTC/peertopeer.js
+++ Videoconference/wwwroot/js/WebRTC/peertopeer.js
@@ -8,8 +8,8 @@ let localStream;
 let peerConnection;
 let peerConnectionConfig = { 'iceServers': [{ 'url': 'stun:stun.services.mozilla.com' }, { 'url': 'stun:stun.l.google.com:19302' }] };
 peerConnection = new RTCPeerConnection(peerConnectionConfig);
-    //peerConnection.onicecandidate = gotIceCandidate;
-    //peerConnection.onaddstream = gotRemoteStream;
+peerConnection.onicecandidate = onDetectIceCandidate;
+peerConnection.onaddstream = gotRemoteStream;
 
 // Cross-platform compatibility
 navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
@@ -47,7 +47,7 @@ async function getUserMedia() {
     }
 }
 
-//SENDER CLIENT
+//PAIRING
 async function invoke_createOffer() {
     try {
         const offer = await peerConnection.createOffer();
@@ -61,18 +61,6 @@ async function invoke_createOffer() {
     } catch (error) {
         console.error('ERROR: creating an offer.', error);
     }
-}
-
-function gotIceCandidate(event) {
-    if (event.candidate != null) {
-        console.log('got candidate');
-        DotNet.invokeMethodAsync('Videoconference', 'SendIceCandidate', JSON.stringify(event.candidate), roomId)
-    }
-}
-
-function gotRemoteStream(event) {
-    console.log('got remote stream');
-    remoteVideo.srcObject = event.stream;
 }
 
 async function invoke_onReceiveOffer(offerString) {
@@ -110,161 +98,23 @@ async function invoke_onReceiveAnswer(answerString) {
     }
 }
 
+//ICE
+function onDetectIceCandidate(event) {
+    if (event.candidate != null) {
+        console.log('Sending new candidate');
+        let candidate = new RTCIceCandidate(event.candidate);
+        DotNet.invokeMethodAsync('Videoconference', 'SendIceCandidate', JSON.stringify(candidate), roomId)
+    }
+}
 
+function invoke_onReceiveIceCandidate(iceCandidateString) {
+    let iceCandidate = new RTCIceCandidate(JSON.parse(iceCandidateString));
+    console.log("Ice candidate received");
 
+    peerConnection.addIceCandidate(iceCandidate);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//OKKKKKKKKKKKKKKKK
-//function gotMessageFromServer(message) {
-//    if (!peerConnection) start(false);
-
-//    var signal = JSON.parse(message.data);
-//    if (signal.sdp) {
-//        peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp), function () {
-//            if (signal.sdp.type == 'offer') {
-//                peerConnection.createAnswer(gotDescription, createAnswerError);
-//            }
-//        });
-//    } else if (signal.ice) {
-//        peerConnection.addIceCandidate(new RTCIceCandidate(signal.ice));
-//    }
-//}
-
-//let remoteStream;
-//let remoteVideo;
-
-//const configuration = { 'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }] }
-//const peerConnection = new RTCPeerConnection(configuration);
-
-//async function initParameters(id) {
-//    roomId = id;
-
-//    remoteVideo = document.querySelector('video#remoteVideo');
-//    //remoteVideo.srcObject = remoteStream;
-//}
-
-////EVENTS
-//peerConnection.ontrack = (event) => {
-//    //remoteStream.addTrack(event.track, remoteStream);
-
-//    if (event.streams && event.streams[0]) {
-//        remoteVideo.srcObject = event.streams[0];
-//    } else {
-//        if (!remoteStream) {
-//            remoteStream = new MediaStream();
-//            remoteVideo.srcObject = remoteStream;
-//        }
-//        remoteStream.addTrack(event.track);
-//    }
-//};
-
-//peerConnection.onicecandidate = (event) => {
-//    if (event.candidate) {
-//        DotNet.invokeMethodAsync('OnlineEducation', 'ShareIceCandidate', JSON.stringify(event.candidate), roomId);
-//        console.log("sharing a candidate");
-//    } else {
-//    /* there are no more candidates coming during this negotiation */
-//        console.log("no more candidate");
-//    }
-//};
-
-//peerConnection.onicegatheringstatechange = (event) => {
-//    let connection = event.target;
-
-//    switch (connection.iceGatheringState) {
-//        case "gathering":
-//            console.log("collection of candidate is occurring");
-//            break;
-//        case "complete":
-//            console.log("collection of candidate is finished");
-//            break;
-//    }
-//}
-
-//peerConnection.onconnectionstatechange = async (event) => {
-//    console.log("State: " + peerConnection.connectionState.toString());
-//    if (peerConnection.connectionState === "connected") {
-//        // Peers connected!
-//        console.log("connected");
-//    }
-//    else if (peerConnection.connectionState === "connecting") {
-//        console.log("connecting");
-//    }
-//    else {
-//        console.log("not connected yet");
-//    }
-//}
-
-////MEDIA
-////play local stream VERY IMPORTANT TO DO BEFORE GETTING ICE CANDIDATES
-//async function webRTC_playVideoFromCamera() {
-//    try {
-//        const constraints = { 'video': true, 'audio': true };
-//        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-//        const videoElement = document.querySelector('video#localVideoPlayback');
-//        videoElement.srcObject = stream;
-
-//        //Adding to the peerconnection
-//        stream.getTracks().forEach(track => {
-//            peerConnection.addTrack(track, stream);
-//        });
-//    } catch (error) {
-//        console.error('Error opening video camera.', error);
-//    }
-//}
-
-////1. Initializing RTCconfiguration
-////2. OnReceivingOffer
-////3. OnReceivingAnswer
-//async function makeCall() {
-//    const offer = await peerConnection.createOffer();
-//    await peerConnection.setLocalDescription(offer);
-
-//    return JSON.stringify(offer);
-//}
-
-//async function onReceivingOffer(offer) {
-//    offer = JSON.parse(offer); //return to object shape
-
-//    const remoteDesc = new RTCSessionDescription(offer);
-//    peerConnection.setRemoteDescription(remoteDesc);
-
-//    const answer = await peerConnection.createAnswer();
-//    await peerConnection.setLocalDescription(answer);
-//    console.log(peerConnection);
-
-//    return JSON.stringify(answer);
-//}
-
-//async function onReceivingAnswer(answer) {
-//    answer = JSON.parse(answer);
-
-//    const remoteDesc = new RTCSessionDescription(answer);
-//    await peerConnection.setRemoteDescription(remoteDesc);
-
-//    console.log(peerConnection);
-//}
-
-////ICE Candidates
-//async function onReceivingIceCandidate(iceCandidate) {
-//    iceCandidate = JSON.parse(iceCandidate);
-//    console.log("State: " + peerConnection.connectionState.toString());
-//    try {
-//        await peerConnection.addIceCandidate(iceCandidate);
-//    } catch (e) {
-//        console.error('Error adding received ice candidate', e);
-//    }
-//}
+function gotRemoteStream(event) {
+    console.log('got remote stream');
+    remoteVideo.srcObject = event.stream;
+}
